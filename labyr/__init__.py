5d00aa474afb8e67892951da374a67b2a73e5e83
sigmacodeslol <m96153045@gmail.com>
2025-06-04 21:41:05 +0100

    generate levels
    made level initiator

--- labyr/__init__.py
+++ labyr/__init__.py
@@ -3,12 +3,38 @@ from typing import Any
 __all__ = ["labyr", "LabyrGame"]
 
 
+def genlvls(chars: dict[str, str], lvlSizes: dict[int, tuple[int, int]]):
+    out = {}
+    SPACE_DEFAULT = " "
+    PLAYER_DEFAULT = "@"
+    WALL_DEFAULT = "#"
+    EXIT_DEFAULT = "E"
+
+    def init(x: int, y: int) -> list[list[str]]:
+        space = chars.get("space", SPACE_DEFAULT)
+        grid = [[space] * y for _ in range(x)]
+
+        for i in range(x):
+            for j in range(y):
+                if i == 0 or i == x - 1 or j == 0 or j == y - 1:
+                    grid[i][j] = "#"
+
+        return grid
+
+    print(init(lvlSizes[0][0], lvlSizes[0][1]))
+
+
 class LabyrGame:
     def __init__(self) -> None:
-        self.tvar = 123
-
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        print("hello from labyr! " + str(self.tvar) + "\nUt et eiusmod enim do dolor.")
+        self.clvl = 0
+        self.chars = {
+            "space": ".",
+            "wall": "#",
+            "player": "@",
+            "exit": "E",
+        }
+        lvlSizes = {0: (3, 5)}
+        levels = genlvls(self.chars, lvlSizes)
 
 
 labyr = LabyrGame()
