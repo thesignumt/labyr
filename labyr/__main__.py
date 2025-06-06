4d17931f7292ab98db65844d305768a598801907
sigmacodeslol <m96153045@gmail.com>
2025-06-06 18:22:25 +0100

    added run arg for script

--- labyr/__main__.py
+++ labyr/__main__.py
@@ -1,12 +1,19 @@
 import argparse
 from importlib.metadata import PackageNotFoundError, version
 
+from .__init__ import labyr
+
 
 def get_version():
     try:
         return version("labyr")  # pypi project name
     except PackageNotFoundError:
         return "unknown"
+
+
+def callback(action):
+    if action == "run":
+        labyr()
 
 
 def main():
@@ -19,7 +26,16 @@ def main():
         help="Show labyr version",
     )
 
+    allowed_actions = ["run"]
+
+    parser.add_argument(
+        "action",
+        choices=allowed_actions,
+        help=f"Action to perform. Allowed values: {', '.join(allowed_actions)}",
+    )
+
     args = parser.parse_args()
+    callback(args.action)
 
 
 if __name__ == "__main__":
