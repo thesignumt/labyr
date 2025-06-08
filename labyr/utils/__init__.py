82f49c8d1db9376afa278a597ffc3dfa129a235c
sigmacodeslol <m96153045@gmail.com>
2025-06-09 00:18:03 +0100

    clsscr()

--- labyr/utils/__init__.py
+++ labyr/utils/__init__.py
@@ -1,9 +1,15 @@
 import msvcrt
+import os
+from os import system
+
+
+def getch():
+    return msvcrt.getch().decode("utf-8")
 
 
 def getchar(d, arg):
     return d.get(arg, d["DEFAULTS"][arg])
 
 
-def getch():
-    return msvcrt.getch().decode("utf-8")
+def clsscr():
+    system("cls" if os.name == "nt" else "clear")
