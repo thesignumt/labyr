e0e98e7e1c1ace36788c1df900a4399d32029d4f
sigmacodeslol <m96153045@gmail.com>
2025-07-04 21:53:08 +0100

    make devtest with a password by environment var

--- labyr/__main__.py
+++ labyr/__main__.py
@@ -1,4 +1,6 @@
 import argparse
+import hashlib
+import os
 from importlib.metadata import PackageNotFoundError, version
 
 
@@ -13,6 +15,18 @@ def run_labyr(level=None):
     from .__init__ import labyr
 
     labyr(level=level)
+
+
+def hash_secret(value: str) -> str:
+    return hashlib.sha256(value.encode()).hexdigest()
+
+
+def dev_test():
+    if (
+        os.environ.get("DEV_TEST_KEY", "")
+        == "9e0fa8e2ae379dc69e30b77507f50075f6d627487f10275a87e3d9c32ac72d71"
+    ):
+        print("success authentication")
 
 
 def main():
@@ -33,12 +47,19 @@ def main():
         metavar="x",
         help="Set the labyrinth level (number)",
     )
+    parser.add_argument("--devtest", action="store_true", help="devtest")
     parser.add_argument(
         "action",
         choices=["run", "nil"],
+        nargs="?",
         help="Action to perform. Allowed values: run, nil",
     )
     args = parser.parse_args()
+    if args.devtest:
+        dev_test()
+        return
+    if not args.action:
+        parser.error("the following arguments are required: action")
     actions = {"run": lambda: run_labyr(level=args.level), "nil": lambda: None}
     actions[args.action]()
 
