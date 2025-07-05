a373ddc5eea3820402f4d37c2a7809c9059e55f7
sigmacodeslol <m96153045@gmail.com>
2025-07-05 21:06:44 +0100

    box function that returns a boxed version of a given string

--- labyr/utils/__init__.py
+++ labyr/utils/__init__.py
@@ -3,6 +3,20 @@ import os
 from os import system
 
 
+def box(string: str) -> str:
+    raw_lines = string.split("\n")
+    stripped_lines = [line.strip() for line in raw_lines]
+    if not stripped_lines:
+        return "+--+\n|  |\n+--+"
+    width = max((len(line) for line in stripped_lines), default=0)
+    border = "+" + "-" * (width + 2) + "+"
+    boxed_lines = [
+        f"| {line.ljust(width)} |" if line else "|" + " " * (width + 2) + "|"
+        for line in stripped_lines
+    ]
+    return f"{border}\n" + "\n".join(boxed_lines) + f"\n{border}"
+
+
 def getch():
     return msvcrt.getch().decode("utf-8")
 
